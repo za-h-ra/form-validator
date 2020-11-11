54329cc50e9538425d57828d0cf18001155b8696
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-10 22:59:14 -0500

    refactor all code

--- js/script.js
+++ js/script.js
@@ -20,27 +20,60 @@ function showSuccess(input) {
 
 // Check Email
 
-function isValidEmail(email) {
+function checkEmail(input) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	return re.test(String(email).toLowerCase())
+	if (re.test(input.value.trim())) {
+		showSuccess(input)
+	} else {
+		showError(input, 'Email is not valid')
+	}
 }
 
 // Check required fields
 
 function checkRequired(inputArr) {
 	inputArr.forEach(function (input) {
-        console.log(input.value)
-        
-        if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`)
-        } else {
-            showSuccess(input)
-        }
+		console.log(input.value)
+
+		if (input.value.trim() === '') {
+			showError(input, `${getFieldName(input)} is required`)
+		} else {
+			showSuccess(input)
+		}
 	})
 }
 
+// Check passwords match
+
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match')
+    }
+}
+
+
+
+
+// Check input length
+
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${getFieldName(input)} must be at least ${min} characters`
+		)
+	} else if (input.value.length > max) {
+		showError(
+			input,
+			`${getFieldName(input)} must be less than ${max} characters`
+		)
+	} else {
+		showSuccess(input)
+	}
+}
+
 function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
 
 // Event Listeners
@@ -49,4 +82,9 @@ form.addEventListener('submit', function (e) {
 	e.preventDefault()
 
 	checkRequired([username, email, password, password2])
+	checkLength(username, 3, 10)
+	checkLength(password, 6, 25)
+	checkLength(password2, 6, 25)
+    checkEmail(email)
+    checkPasswordsMatch(password, password2)
 })
