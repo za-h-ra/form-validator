0229276b930c190a240fafadd334cccecf8e204e
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-10 22:45:34 -0500

    refactor javascript

--- js/script.js
+++ js/script.js
@@ -21,8 +21,26 @@ function showSuccess(input) {
 // Check Email
 
 function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	return re.test(String(email).toLowerCase())
+}
+
+// Check required fields
+
+function checkRequired(inputArr) {
+	inputArr.forEach(function (input) {
+        console.log(input.value)
+        
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+	})
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
 
 // Event Listeners
@@ -30,29 +48,5 @@ function isValidEmail(email) {
 form.addEventListener('submit', function (e) {
 	e.preventDefault()
 
-	if (username.value === '') {
-		showError(username, 'Username is required')
-	} else {
-		showSuccess(username)
-	}
-
-    if (email.value === '') {
-        showError(email, 'Email is required')
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-	} else {
-		showSuccess(email)
-	}
-
-	if (password.value === '') {
-		showError(password, 'Password is required')
-	} else {
-		showSuccess(password)
-	}
-
-	if (password2.value === '') {
-		showError(password2, 'Please confirm password')
-	} else {
-		showSuccess(password2)
-	}
+	checkRequired([username, email, password, password2])
 })
