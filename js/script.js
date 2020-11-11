c0bf5cb142ede74ca48b420e402b6fe0ed5645ba
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-10 22:34:29 -0500

    check if email is valid

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const form = document.getElementById('form')
 const username = document.getElementById('username')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
@@ -7,24 +7,52 @@ const password2 = document.getElementById('password2')
 // Functions
 
 function showError(input, message) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control error'
-    const small = formControl.querySelector('small')
-    small.innerText = message
+	const formControl = input.parentElement
+	formControl.className = 'form-control error'
+	const small = formControl.querySelector('small')
+	small.innerText = message
 }
 
+function showSuccess(input) {
+	const formControl = input.parentElement
+	formControl.className = 'form-control success'
+}
 
+// Check Email
+
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	return re.test(String(email).toLowerCase())
+}
 
 // Event Listeners
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault()
+	e.preventDefault()
 
-    if (username.value === '') {
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username)
-    }
+	if (username.value === '') {
+		showError(username, 'Username is required')
+	} else {
+		showSuccess(username)
+	}
 
-    console.log(username.value)
+    if (email.value === '') {
+        showError(email, 'Email is required')
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid')
+	} else {
+		showSuccess(email)
+	}
+
+	if (password.value === '') {
+		showError(password, 'Password is required')
+	} else {
+		showSuccess(password)
+	}
+
+	if (password2.value === '') {
+		showError(password2, 'Please confirm password')
+	} else {
+		showSuccess(password2)
+	}
 })
